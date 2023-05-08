541f9d9fe840f1b2cdd951138fd15d49daea95df
EliziejusC <115976100+EliziejusC@users.noreply.github.com>
2023-05-08 12:25:40 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
-// Import stylesheets
 import './index.scss';
 
-// Write TypeScript code!
 export class SevenSegmentDisplay {
-  numbers = {};
   Asegment = document.getElementsByClassName('display__segment A');
   Bsegment = document.getElementsByClassName('display__segment B');
   Csegment = document.getElementsByClassName('display__segment C');
@@ -12,54 +9,63 @@ export class SevenSegmentDisplay {
   Fsegment = document.getElementsByClassName('display__segment F');
   Gsegment = document.getElementsByClassName('display__segment G');
   regexValidator = '[0-9]';
-
+  textContent = document.getElementById('number') as HTMLInputElement;
   constructor() {
     let button = document.getElementById('btn');
     button.addEventListener('click', () => this.save());
   }
+  
+
+  arraySplit() {
+    const textArray = [];
+    textArray.push(this.textContent.value);
+    const splitArray = textArray[0].split("")
+    return splitArray;
+  }
 
   save(): void {
-    const textContent = document.getElementById('number') as HTMLInputElement;
-    const textValue = textContent.value;
-    if(textContent.value.match(this.regexValidator)){
-      if (textValue === '1') {
+    
+    const numberValue = this.arraySplit();  
+    console.log(numberValue[0])
+    if(this.textContent.value.match(this.regexValidator)){
+      if (numberValue[0] === '1') {
         this.Bsegment[0].classList.add('red');
         this.Csegment[0].classList.add('red');
-      } else if (textValue === '2') {
+      } else if (numberValue[0] === '2') {
         this.Asegment[0].classList.add('red');
         this.Bsegment[0].classList.add('red');
         this.Gsegment[0].classList.add('red');
         this.Esegment[0].classList.add('red');
         this.Dsegment[0].classList.add('red');
-      } else if (textValue === '3') {
+      } else if (numberValue[0] === '3') {
         this.Asegment[0].classList.add('red');
         this.Bsegment[0].classList.add('red');
         this.Gsegment[0].classList.add('red');
         this.Csegment[0].classList.add('red');
         this.Dsegment[0].classList.add('red');
-      } else if (textValue === '4') {
+      } else if (numberValue[0] === '4') {
         this.Fsegment[0].classList.add('red');
         this.Bsegment[0].classList.add('red');
         this.Gsegment[0].classList.add('red');
         this.Csegment[0].classList.add('red');
-      } else if (textValue === '5') {
+      } else if (numberValue[0] === '5') {
         this.Asegment[0].classList.add('red');
         this.Fsegment[0].classList.add('red');
         this.Gsegment[0].classList.add('red');
         this.Csegment[0].classList.add('red');
         this.Dsegment[0].classList.add('red');
-      } else if (textValue === '6') {
+      } else if (numberValue[0] === '6') {
         this.Asegment[0].classList.add('red');
         this.Csegment[0].classList.add('red');
         this.Fsegment[0].classList.add('red');
         this.Esegment[0].classList.add('red');
         this.Dsegment[0].classList.add('red');
         this.Gsegment[0].classList.add('red');
-      } else if (textValue === '7') {
+      } else if (numberValue[0] === '7') {
         this.Asegment[0].classList.add('red');
         this.Bsegment[0].classList.add('red');
         this.Csegment[0].classList.add('red');
-      } else if (textValue === '8') {
+      } else if (numberValue[0] === '8') {
         this.Asegment[0].classList.add('red');
         this.Bsegment[0].classList.add('red');
         this.Csegment[0].classList.add('red');
@@ -67,13 +73,66 @@ export class SevenSegmentDisplay {
         this.Esegment[0].classList.add('red');
         this.Fsegment[0].classList.add('red');
         this.Gsegment[0].classList.add('red');
-      } else if (textValue === '9') {
+      } else if (numberValue[0] === '9') {
         this.Asegment[0].classList.add('red');
         this.Bsegment[0].classList.add('red');
         this.Csegment[0].classList.add('red');
         this.Dsegment[0].classList.add('red');
         this.Fsegment[0].classList.add('red');
         this.Gsegment[0].classList.add('red');
+      }
+      if (numberValue[1] === '1') {
+        this.Bsegment[1].classList.add('red');
+        this.Csegment[1].classList.add('red');
+      } else if (numberValue[1] === '2') {
+        this.Asegment[1].classList.add('red');
+        this.Bsegment[1].classList.add('red');
+        this.Gsegment[1].classList.add('red');
+        this.Esegment[1].classList.add('red');
+        this.Dsegment[1].classList.add('red');
+      } else if (numberValue[1] === '3') {
+        this.Asegment[1].classList.add('red');
+        this.Bsegment[1].classList.add('red');
+        this.Gsegment[1].classList.add('red');
+        this.Csegment[1].classList.add('red');
+        this.Dsegment[1].classList.add('red');
+      } else if (numberValue[1] === '4') {
+        this.Fsegment[1].classList.add('red');
+        this.Bsegment[1].classList.add('red');
+        this.Gsegment[1].classList.add('red');
+        this.Csegment[1].classList.add('red');
+      } else if (numberValue[1] === '5') {
+        this.Asegment[1].classList.add('red');
+        this.Fsegment[1].classList.add('red');
+        this.Gsegment[1].classList.add('red');
+        this.Csegment[1].classList.add('red');
+        this.Dsegment[1].classList.add('red');
+      } else if (numberValue[1] === '6') {
+        this.Asegment[1].classList.add('red');
+        this.Csegment[1].classList.add('red');
+        this.Fsegment[1].classList.add('red');
+        this.Esegment[1].classList.add('red');
+        this.Dsegment[1].classList.add('red');
+        this.Gsegment[1].classList.add('red');
+      } else if (numberValue[1] === '7') {
+        this.Asegment[1].classList.add('red');
+        this.Bsegment[1].classList.add('red');
+        this.Csegment[1].classList.add('red');
+      } else if (numberValue[1] === '8') {
+        this.Asegment[1].classList.add('red');
+        this.Bsegment[1].classList.add('red');
+        this.Csegment[1].classList.add('red');
+        this.Dsegment[1].classList.add('red');
+        this.Esegment[1].classList.add('red');
+        this.Fsegment[1].classList.add('red');
+        this.Gsegment[1].classList.add('red');
+      } else if (numberValue[1] === '9') {
+        this.Asegment[1].classList.add('red');
+        this.Bsegment[1].classList.add('red');
+        this.Csegment[1].classList.add('red');
+        this.Dsegment[1].classList.add('red');
+        this.Fsegment[1].classList.add('red');
+        this.Gsegment[1].classList.add('red');
       }
     } else {
       const error = document.getElementsByClassName('none-active-error');
